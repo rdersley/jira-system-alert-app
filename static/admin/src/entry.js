1e9a1b896fe0baa54e665754ad458be1d25cdb8a
rdersley <rdersley@yahoo.com>
2026-09-02 06:56:43 +0100

    Load monthly test admin tools

--- static/admin/src/entry.js
+++ static/admin/src/entry.js
@@ -1,6 +1,7 @@
 import './contact-form-guard.js';
 import './main.js';
 import './contact-multiclient.js';
+import './monthly-test-tools.js';
 import './setup-wizard.js';
 import './setup-wizard-overrides.css';
 import './release-polish.css';
